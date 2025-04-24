10588a7d584f12a17c616ce093084e2964db90d1
Cam Lavoie <lavo0194@algonquinlive.com>
2025-04-23 20:19:00 -0400

    Fixing issues

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
-  publicPath: '/mtm6407-headless-cms-lavo0194/',
+  publicPath: '/mtm6407-headless-cms1-lavo0194/',
   transpileDependencies: true
 })
 
